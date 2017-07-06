824dcb136ad859b3de584527963483d8fe1e33a0
jereromanoo <jere0699@gmail.com>
2017-07-06 12:42:44 -0300

    categorias forma de boton

--- steam/urls.py
+++ steam/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^', views.juego_list),
-    url(r'juego/(?P<pk>[0-9]+)/$', views.listado_categoria, name='cat'),
+    url(r'categoria/(?P<pk>[0-9]+)/$', views.categoria_list, name='cat'),
 ]
 
 
--- steam/views.py
+++ steam/views.py
@@ -7,9 +7,9 @@ def juego_list(request):
     generos = Genero.objects.all()
     return render(request, 'steam/juego_list.html', {'generos': generos})
 
-def listado_categoria(request,pk):
+def categoria_list(request,pk):
     juegos=Juego.objetcs.filter(genero=pk)
-    return render(request, 'steam/categoria_list.html', {'juegos': juegos})
+    return render(request, 'steam/juego_list.html', {'juegos': juegos})
         
 
 
