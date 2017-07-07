1837668704ad8188a37ab80e187b7ac9db444ca0
thevegit0 <taieltraversa@gmail.com>
2017-07-07 10:54:17 -0300

    arreglar categorias

--- steam/urls.py
+++ steam/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^', views.juego_list),
+    url(r'^$', views.juego_list),
     url(r'categoria/(?P<pk>[0-9]+)/$', views.categoria_list, name='cat'),
 ]
 
--- steam/views.py
+++ steam/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 from django.utils import timezone
-from .models import Genero
-from .models import Juego
+from .models import Genero, Juego
 
 def juego_list(request):
     generos = Genero.objects.all()
     return render(request, 'steam/juego_list.html', {'generos': generos})
 
 def categoria_list(request,pk):
-    juegos=Juego.objetcs.filter(genero=pk)
-    return render(request, 'steam/juego_list.html', {'juegos': juegos})
+    juegos=Juego.objects.filter(genero=pk)
+    return render(request, 'steam/categoria_list.html', {'juegos': juegos})
         
 
 
